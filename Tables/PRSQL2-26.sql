7b99f55cc5051be1cb134acb7be7c8b23a4e031e
KaterynaPost <148450889+KaterynaPost@users.noreply.github.com>
2023-10-25 13:33:23 +0300

    Update PRSQL2-26.sql

--- Tables/PRSQL2-26.sql
+++ Tables/PRSQL2-26.sql
@@ -1,5 +1,4 @@
 create or replace PROCEDURE del_employee(
-    p_employee_id NUMBER,
     p_first_name VARCHAR2,
     p_last_name VARCHAR2,
     p_job_id NUMBER,
@@ -7,6 +6,7 @@ create or replace PROCEDURE del_employee(
     p_phone_number VARCHAR2,
     p_hire_date DATE,
     p_salary NUMBER
+    p_employee_id NUMBER
 ) IS
     l_employee_count NUMBER;
 BEGIN
@@ -14,7 +14,6 @@ BEGIN
     SELECT COUNT(*)
     INTO l_employee_count
     FROM proj.employees
-    WHERE employee_id = p_employee_id
       AND first_name = p_first_name
       AND last_name = p_last_name
       AND job_id = p_job_id
@@ -22,9 +21,12 @@ BEGIN
       AND phone_number = p_phone_number
       AND hire_date = p_hire_date
       AND salary = p_salary;
+    WHERE employee_id = p_employee_id;
+      
 
     IF l_employee_count = 0 THEN
         raise_application_error(-20002, 'Значення з параметрів не знайдено');
+        raise_application_error(-20002, '�������� � ��������� �� ��������');
     ELSE
         DELETE FROM proj.employees
         WHERE employee_id = p_employee_id
@@ -34,4 +36,6 @@ EXCEPTION
     WHEN OTHERS THEN
         -- Обробка інших помилок
         DBMS_OUTPUT.PUT_LINE('Помилка: ' || SQLERRM);
+      
+        DBMS_OUTPUT.PUT_LINE('�������: ' || SQLERRM);
 END;
