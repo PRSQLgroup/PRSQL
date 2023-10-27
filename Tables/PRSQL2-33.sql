bb06bc1573f00eb69ec90cfae03666124581b6de
SemenenkoDmytro <148434464+SemenenkoDmytro@users.noreply.github.com>
2023-10-27 15:16:36 +0300

    Create PRSQL2-33.sql

---
+++ Tables/PRSQL2-33.sql
@@ -0,0 +1,15 @@
+create table proj.currency (
+    currency_id number,
+    currency_txt varchar2(3),
+    currency_value number,
+    currency_date date default sysdate); 
+
+
+create table proj.currency_history (
+    currency_id number,
+    currency_txt varchar2(3),
+    currency_value number,
+    start_date date,
+    end_date date,
+    currency_date date default sysdate);
+
