c162149a46b246aa4455ebb161b6d35f85a72daf
PRSQLgroup <148758794+PRSQLgroup@users.noreply.github.com>
2023-10-28 13:48:35 +0300

    Update currency.sql

--- Tables/currency.sql
+++ Tables/currency.sql
@@ -5,11 +5,5 @@ create table proj.currency (
     currency_date date default sysdate); 
 
 
-create table proj.currency_history (
-    currency_id number,
-    currency_txt varchar2(3),
-    currency_value number,
-    start_date date,
-    end_date date,
-    currency_date date default sysdate);
+
 
