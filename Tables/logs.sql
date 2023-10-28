5a761be6b1849da3eeea4899f5851c29a81a8bc0
mrmadoc <mrmadoc007@gmail.com>
2023-10-28 17:13:08 +0300

    PRSQL2-32

--- Tables/logs.sql
+++ Tables/logs.sql
@@ -2,7 +2,7 @@ create table proj.logs(
                id number generated always as identity minvalue 1 maxvalue 9999999999, 
                appl_proc varchar2(50), 
                message varchar2(200), 
-               log_date date Default Sysdate, 
+               log_date date default sysdate, 
                user_os varchar2(50) default sys_context('userenv', 'os_user'),
                user_db varchar2(50) Default sys_context('userenv', 'session_user')
                );
