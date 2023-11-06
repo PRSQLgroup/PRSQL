40fef9658bbf5a6b63294af89bd8acdf898c96b1
SemenenkoDmytro <148434464+SemenenkoDmytro@users.noreply.github.com>
2023-11-07 00:57:37 +0200

    PRSQL2-50

---
+++ Views/currencies_v.sql
@@ -0,0 +1,24 @@
+create or replace view currencies_v as
+select
+  gt.r030,
+  gt.txt,
+  gt.rate,
+  gt.cc,
+  to_date(gt.exchangedate, 'DD.MM.YYYY') as exchangedate
+from (
+  select sys.get_nbu_currencies(p_date => sysdate-10 ) as json_by_curency
+  from dual
+) tt
+cross join json_table (
+  tt.json_by_curency, '$[*]'
+  columns (
+    r030 number path '$.r030',
+    txt varchar2(100) path '$.txt',
+    rate number path '$.rate',
+    cc varchar2(3) path '$.cc',
+    exchangedate varchar2(10) path '$.exchangedate'
+  )
+) gt;
+
+
+
