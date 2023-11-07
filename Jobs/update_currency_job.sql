97577289375ebb1cab24a0e1ee97f88d2188ce4d
mrmadoc <mrmadoc007@gmail.com>
2023-11-07 21:21:42 +0200

    PRSQL2-53

--- Jobs/update_currency_job.sql
+++ Jobs/update_currency_job.sql
@@ -3,7 +3,7 @@ BEGIN
     job_name        => 'UPDATE_CURRENCY_JOB',
     job_type        => 'PLSQL_BLOCK',
     job_action      => 'BEGIN set_currency; END;',
-    start_date      => '09-NOV-23 07:00:00',
+    start_date      => '09-11-23 07:00:00',
     repeat_interval => 'FREQ=DAILY; BYHOUR=7',
     enabled         => TRUE,
     comments        => 'Оновлення курсів валют щоранку о 7:00'
